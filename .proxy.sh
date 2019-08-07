b3a7ada98554da22a025c2405f0ee35be4dea897
everton-canez <everton-canez@procergs.rs.gov.br>
2019-08-07 11:19:37 -0300

    Much simpler way using one argument only. ex: ./.proxy.sh user:password@proxy.mycompany.com:9999

--- .proxy.sh
+++ .proxy.sh
@@ -4,49 +4,48 @@
 # npm config settings via command line to 
 # work deal with npm downloads.
 # Example usage: 
-#   ./proxy.sh ./.proxy.sh smeagol:myprecious123@proxy.mycompany.com:9999
+#   ./proxy.sh ./.proxy.sh user:password@proxy.mycompany.com:9999
 
-tput setaf 2
-if [ ! $1 ] || [ ! $2 ] || [ ! $3 ] || [ ! $4 ]; then
-    clear
-    tput setaf 1
-    echo "**** All parameters must be passed, ex.: "
-    tput setaf 3
+if [ $1 == "--help" ]; then
     echo "   ==> source ./.proxy.sh username password proxy-url proxy-port" 
-    tput setaf 2
 else
-    # arguments
-    user=$1;
-    pass=$2;
-    url=$3;
-    port=$4;
-
-    # final proxy url  
-    url=$user:$pass@$url:$port;
-
-    echo $url;
-
-    # unset variables before set them again
-    source ./.unproxy.sh
-
-    # set variables for a new proxy session
-    export HTTP_PROXY=http://$url
-    export HTTPS_PROXY=https://$url
-    export IONIC_HTTP_PROXY=http://$url
-    npm config set https-proxy http://$url
-    npm config set proxy http://$url
-    npm config set strict-ssl false
-
-    clear
-    tput setaf 4
-    echo "**** Proxy environment settings ok."
-    tput setaf 3
-    env | grep "PROXY"
-
-    tput setaf 4
-    echo "**** NPM environment settings ok."
-    tput setaf 3
-    npm config get | grep "proxy"
     tput setaf 2
-fi
+    if [ ! $1 ]; then
+        clear
+        tput setaf 1
+        echo "**** You must be pass an argument like this, ex.: "
+        tput setaf 3
+        echo "   ==> ./proxy.sh ./.proxy.sh user:password@proxy.mycompany.com:9999" 
+        tput setaf 2
+    else
+        # arguments
+        url=$1;
 
+        tput setaf 2
+        echo Proxy URL:
+        echo $url;
+
+        # unset variables before set them again
+        source ./.unproxy.sh
+
+        # set variables for a new proxy session
+        export HTTP_PROXY=http://$url
+        export HTTPS_PROXY=https://$url
+        export IONIC_HTTP_PROXY=http://$url
+        npm config set https-proxy http://$url
+        npm config set proxy http://$url
+        npm config set strict-ssl false
+
+        clear
+        tput setaf 4
+        echo "**** Proxy environment settings ok."
+        tput setaf 3
+        env | grep "PROXY"
+
+        tput setaf 4
+        echo "**** NPM environment settings ok."
+        tput setaf 3
+        npm config get | grep "proxy"
+        tput setaf 2
+    fi
+fi
